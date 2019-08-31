1fd53a23e741d7a8a473fc2bf3b1e0dc13accaaf
Dao <ttdao.2015@accountancy.smu.edu.sg>
2019-09-01 00:07:35 +0800

    add in tox + circleCI

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="mlpipeline",
+        version="0.1",
+        author="Thanh Tung Dao",
+        author_email="dttung2905@gmail.com",
+        description="Fast developement of machine learning pipeline",
+        packages=find_packages(),
+        install_requires=[
+            'flake8==3.7.8',
+            'matplotlib==3.1.1',
+            'numpy==1.17.1',
+            'pandas==0.25.1',
+            'pytest==5.1.2',
+            'python-dateutil==2.8.0',
+            'pytest-cov==2.7.1'
+            ]
+
+        )
