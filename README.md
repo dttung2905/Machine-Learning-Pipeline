093fff271d42b9489a2385890406a9b6d7c75068
Dao <ttdao.2015@accountancy.smu.edu.sg>
2019-09-01 21:36:02 +0800

    done with aggregation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Machine-Learning-Pipeline
-[![CircleCI](https://circleci.com/gh/dttung2905/Machine-Learning-Pipeline/tree/master.svg?style=svg)](https://circleci.com/gh/dttung2905/Machine-Learning-Pipeline/tree/master) [![codecov](https://codecov.io/gh/dttung2905/Machine-Learning-Pipeline/branch/master/graph/badge.svg)](https://codecov.io/gh/dttung2905/Machine-Learning-Pipeline)
+[![CircleCI](https://circleci.com/gh/dttung2905/Machine-Learning-Pipeline/tree/master.svg?style=svg)](https://circleci.com/gh/dttung2905/Machine-Learning-Pipeline/tree/master) [![codecov](https://codecov.io/gh/dttung2905/Machine-Learning-Pipeline/branch/master/graph/badge.svg)](https://codecov.io/gh/dttung2905/Machine-Learning-Pipeline) [![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/dwyl/esta/issues)
 
 
 A repo for useful Machine Learning code. The aim is to reduce the amount of code time, to increase flexibility and reusability of the code 
--- tests/processing/test_aggregation.py
+++ tests/processing/test_aggregation.py
@@ -1,5 +1,8 @@
 import pytest
-from mlpipeline.processing.aggregation import create_new_col
+from mlpipeline.processing.aggregation import \
+        create_new_col, custom_aggregation
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
 
 
 def test_create_new_col():
@@ -12,3 +15,22 @@ def test_create_new_col():
     expected_output = ['height_max', 'height_min', 'weight_sum']
 
     assert output == expected_output
+
+
+def test_custom_aggregation():
+    '''
+    test the custom_aggregation() function
+    '''
+    input_df = {'colA': [1, 2, 3, 4],
+                'colB': [3, 4, 2, 1],
+                'colC': ['X', 'X', 'Y', 'Y']}
+    input_df = pd.DataFrame(data=input_df)
+    agg_col = 'colC'
+    config = {'colA': ['mean', 'sum'], 'colB': ['max']}
+    return_df = custom_aggregation(input_df, agg_col, config)
+    expected_df = {'colC': ['X', 'Y'],
+                   'colA_mean': [1.5, 3.5],
+                   'colA_sum': [3, 7],
+                   'colB_max': [4, 2]}
+    expected_df = pd.DataFrame(data=expected_df)
+    assert_frame_equal(expected_df, return_df)
